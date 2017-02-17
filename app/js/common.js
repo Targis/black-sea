b068c8d6ecefc1d53155bc1fc13c4d9659047ada
Yuri Kozyk <y.kozyk@gmail.com>
2017-02-17 22:01:22 +0200

    Added mobile menu and media queries

--- app/js/common.js
+++ app/js/common.js
@@ -5,4 +5,17 @@ $(function() {
 		pager: true,
 	});
 
+	// Mobile menu
+	$(".toggle-menu, .mobile-menu li, wrapper-invis").click(function() {
+		$(".sandwich").toggleClass("active");
+		$(".mobile-menu").slideToggle();
+	});
+
+	$(document).click(function(e){
+
+		if($(e.target).closest('.mobile-menu-wrapper').length != 0) return false;
+		$(".sandwich").removeClass("active");
+		$(".mobile-menu").hide();
+	});
+
 });
